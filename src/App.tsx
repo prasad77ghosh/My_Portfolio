59d34e550fe8f5295c0a696b5f241d9fddf1a839
prasad77ghosh <prasadghosh997@gmail.com>
2023-09-15 08:01:59 +0530

    splash screen mechanism complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { About, Contact, Home, Portfolio } from "./pages";
-
+import { About, Article, Contact, Home, Portfolio, Resume } from "./pages";
+import { useState, useEffect } from "react";
+import { SplashScreen } from "./components";
 const App = () => {
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    let timeOutId: number;
+    const splashShown = sessionStorage.getItem("splashShown");
+    if (splashShown) {
+      setShowSplash(false);
+    } else {
+      timeOutId = setTimeout(() => {
+        setShowSplash(false);
+        sessionStorage.setItem("splashShown", "true");
+      }, 3000);
+    }
+
+    return () => {
+      if (timeOutId) {
+        clearTimeout(timeOutId);
+      }
+    };
+  }, [showSplash]);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={showSplash ? <SplashScreen /> : <Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,5 @@ export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
 export { default as Contact } from "./Contact/Contact";
 export { default as Portfolio } from "./Portfolio/Portfolio";
+export { default as Resume } from "./Resume/Resume";
+export { default as Article } from "./Article/Article";
